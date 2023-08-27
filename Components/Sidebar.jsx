30073db4fd4c04f58547d392a3b47e2393488e1c
Nishant Choudhary <nishantchoudhary1403@gmail.com>
2023-08-27 16:29:47 +0530

    optimised sidebar

--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -5,25 +5,26 @@ import classNames from 'classnames'
 import Link from 'next/link'
 
 const menuItems = [
-    {id:1, label:"TouristSpots", icon: <ion-icon name="restaurant-outline"></ion-icon>, link:"/TouristSpots"},
-    {id:2, label:"Temples",      icon: <ion-icon name="restaurant-outline"></ion-icon>, link:"/Temples"},
+    {id:1, label:"TouristSpots", icon: <ion-icon name="pin"></ion-icon>, link:"/TouristSpots"},
+    {id:2, label:"Temples",      icon: <ion-icon name="bandage-outline"></ion-icon>, link:"/Temples"},
     {id:3, label:"Restaurants",  icon: <ion-icon name="restaurant-outline"></ion-icon>, link:"/Restaurants"},
-    {id:4, label:"Hotels",       icon: <ion-icon name="restaurant-outline"></ion-icon>, link:"/Hotels"},
+    {id:4, label:"Hotels",       icon: <ion-icon name="bed-outline"></ion-icon>, link:"/Hotels"},
 ]
 
 const Sidebar = () => {
 
-    const [toggleCollapse, setToggleCollapse] = useState(false);
+    const [toggleCollapse, setToggleCollapse] = useState(true);
 
     const onMouseOver = () => {
         setToggleCollapse(!toggleCollapse);
     };
 
-    const sidebarClasses = () => {
-        return classNames(
-          "flex items-center cursor-pointer hover:bg-light-lighter rounded w-full overflow-hidden whitespace-nowrap",
+    const sidebarClasses = classNames(
+            " flex flex-col items-center tracking-wide gap-2 py-2 px-1 md:px-2 text-xl md:text-3xl font-semibold bg-gray-100 drop-shadow-md transition-all ease-in-out duration-200",
+            { 'md:w-[5%] w-[12%]':[!toggleCollapse],
+              'md:w-[30%] w-[50%]':[toggleCollapse]
+        },
         );
-      };
 
   return (
     <div 
@@ -31,25 +32,21 @@ const Sidebar = () => {
         onMouseEnter={onMouseOver}
         onMouseLeave={onMouseOver}
     >
-        <div className='flex flex-col'>
+        <div className="w-full h-screen bg-gray-100 drop-shadow-md  transition-all ease-in-out duration-500">
             <div className='flex items-center justify-between relative'>
-                <div className='flex items-center pl-1 gap-4'>
-                    {toggleCollapse && (
-                        <button onClick={onMouseOver}>
-                            <ion-icon name="chevron-forward-circle-outline"></ion-icon>            
-                        </button>
-                    )}
+                <div className={` justify-end mx-2 my-2 ${!toggleCollapse ? 'rotate-180': 'rotate-0'}}`}>
+                    <button className='text-2xl font-bold' onClick={onMouseOver}>
+                        <ion-icon name="chevron-forward-circle-outline"></ion-icon>            
+                    </button>
                 </div>
-
             </div>
-            <div className='flex flex-col items-start'>
-                {menuItems.map(({icon:Icon, ...menu}) => {
-                    const classes= sidebarClasses();
+            <div className='flex flex-col gap-1 md:gap-2 px-1 items-start'>
+                {menuItems.map(({...menu}) => {
                     return (
-                        <div key={menu.key} className={classes}>
-                            <Link href={menu.link}>
-                                    <div>
-                                        <Icon />
+                        <div key={menu.key} className=" border border-gray-200 drop-shadow-sm rounded-md w-[100%] py-3 md:py-5 px-2 md:px-10">
+                            <Link className='flex flex-row gap-2 md:gap-4' href={menu.link}>
+                                    <div className='text-lg md:text-2xl font-bold'>
+                                        <h1>{menu.icon}</h1>
                                     </div>
                                     {!toggleCollapse && (
                                         <span>
